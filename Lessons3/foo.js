87c6fcd30e38008c1171f5a5dd72607eee7e516e
mc227 <markcostales.mark@gmail.com>
2023-02-21 23:19:25 -0500

    added foo.js

---
+++ Lessons3/foo.js
@@ -0,0 +1,24 @@
+// var myCircle = d3.select('circle')
+
+// myCircle.attr('fill','red')
+//     .attr('r',10)
+//     .attr('stroke','black')
+//     .attr('stroke-width',2)
+
+// var myDiv = d3.select('div')
+
+// myDiv.style('background-color','blue')
+//     .select('p')
+//         .style('font-family','Helvetica')
+
+// var myGraf = d3.select('div')
+//         .style('background-color','blue')
+//     .select('p')
+//         .style('font-family','Helvetica')
+
+var myDiv = d3.select('div');
+
+myDiv.append('p')
+    .style('font-style','italic')
+    .style('color','grey')
+    .text('Oh Hi Mark!!!')
